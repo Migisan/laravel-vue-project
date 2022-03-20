9e1f200a570510e7e0a709cc518b9b940f8aa33b
Migisan <ukyo.business.desktop@gmail.com>
2022-03-20 22:15:39 +0900

    ArticleStoreApiTestのリファクタリング

--- src/tests/Feature/ArticleStoreApiTest.php
+++ src/tests/Feature/ArticleStoreApiTest.php
@@ -14,6 +14,10 @@ class ArticleStoreApiTest extends TestCase
     // テスト後のデータベースリセット
     use RefreshDatabase;
 
+    private $user;
+
+    private $datetime_format;
+
     /**
      * テスト前処理
      *
@@ -25,6 +29,9 @@ class ArticleStoreApiTest extends TestCase
 
         // テストユーザーの作成
         $this->user = factory(User::class)->create();
+
+        // 日時フォーマット
+        $this->datetime_format = config('const.DATETIME_FORMAT');
     }
 
     /**
@@ -49,8 +56,39 @@ class ArticleStoreApiTest extends TestCase
         // 登録したデータ取得
         $article = Article::first();
 
+        // 期待値
+        $expected_structure = [
+            'id',
+            'title',
+            'body',
+            'updated_at',
+            'user' => [
+                'id',
+                'name',
+                'email',
+                'image_path',
+                'updated_at',
+            ],
+        ];
+        $expected_data = [
+            'id' => $article->id,
+            'title' => $article->title,
+            'body' => $article->body,
+            'updated_at' => $article->updated_at->format($this->datetime_format),
+            'user' => [
+                'id' => $article->user->id,
+                'name' => $article->user->name,
+                'email' => $article->user->email,
+                'image_path' => $article->user->image_path,
+                'updated_at' => $article->user->updated_at->format($this->datetime_format),
+            ],
+        ];
+
         // 検証
-        $response->assertStatus(201);
-        $this->assertEquals($article->title, $data['title']);
+        $response->assertStatus(201)
+            ->assertJsonStructure($expected_structure)
+            ->assertExactJson($expected_data);
+        $this->assertSame($article->title, $data['title']);
+        $this->assertSame($article->body, $data['body']);
     }
 }
