a734186ebf0c3e51171d8aa02492b4d9c712c383
Migisan <ukyo.business.desktop@gmail.com>
2022-03-20 23:22:10 +0900

    UserDetailApiTestのリファクタリング

--- src/tests/Feature/UserDetailApiTest.php
+++ src/tests/Feature/UserDetailApiTest.php
@@ -14,6 +14,9 @@ class UserDetailApiTest extends TestCase
     // テスト後のデータベースリセット
     use RefreshDatabase;
 
+    private $user;
+    private $datetime_format;
+
     /**
      * テスト前処理
      *
@@ -22,6 +25,20 @@ class UserDetailApiTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
+
+        // ユーザーデータ生成
+        $users = factory(User::class, 3)->create();
+        $this->user = $users->first();
+
+        // 記事データ生成
+        foreach ($users as $user) {
+            factory(Article::class, 3)->create([
+                'user_id' => $user->id,
+            ]);
+        }
+
+        // 日付フォーマット
+        $this->datetime_format = config('const.DATETIME_FORMAT');
     }
 
     /**
@@ -30,34 +47,72 @@ class UserDetailApiTest extends TestCase
      * @test
      * @return void
      */
-    public function getUser()
+    public function getUser(): void
     {
-        // ユーザーデータ生成
-        $user = factory(User::class)->create();
-        // 記事データ生成
-        factory(Article::class, 10)->create();
-
         // レスポンス
-        $response = $this->json('GET', route('users.show', $user->id));
+        $response = $this->json('GET', route('users.show', $this->user->id));
         $response->dump();
 
         // 生成した記事データ取得
-        $articles = Article::with(['user'])->orderBy('updated_at', 'desc')->get();
+        $articles = Article::with(['user'])
+            ->where('user_id', $this->user->id)
+            ->orderBy('updated_at', 'desc')
+            ->get();
 
-        // 検証
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                'user' => [
-                    'name' => $user->name,
-                    'email' => $user->email,
-                ],
-                'articles' => $articles->only([
+        // 期待値
+        $expected_structure = [
+            'user' => [
+                'id',
+                'name',
+                'email',
+                'image_path',
+                'updated_at',
+            ],
+            'articles' => [
+                '*' => [
                     'id',
                     'title',
                     'body',
                     'updated_at',
-                ])->toArray(),
+                    'user' => [
+                        'id',
+                        'name',
+                        'email',
+                        'image_path',
+                        'updated_at',
+                    ],
+                ],
+            ],
+        ];
+        $expected_user = [
+            'id' => $this->user->id,
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'image_path' => $this->user->image_path,
+            'updated_at' => $this->user->updated_at->format($this->datetime_format),
+        ];
+        $expected_articles = $articles->map(function ($article) {
+            return [
+                'id' => $article->id,
+                'title' => $article->title,
+                'body' => $article->body,
+                'updated_at' => $article->updated_at->format($this->datetime_format),
+                'user' => [
+                    'id' => $article->user->id,
+                    'name' => $article->user->name,
+                    'email' => $article->user->email,
+                    'image_path' => $article->user->image_path,
+                    'updated_at' => $article->user->updated_at->format($this->datetime_format),
+                ],
+            ];
+        })->all();
+
+        // 検証
+        $response->assertStatus(200)
+            ->assertJsonStructure($expected_structure)
+            ->assertExactJson([
+                'user' => $expected_user,
+                'articles' => $expected_articles,
             ]);
     }
 }
