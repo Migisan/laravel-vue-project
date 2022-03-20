7323fa210ff27eed097732705c67be186c4775b9
Migisan <ukyo.business.desktop@gmail.com>
2022-03-20 22:48:12 +0900

    RegisterApiTestのリファクタリング

--- src/tests/Feature/RegisterApiTest.php
+++ src/tests/Feature/RegisterApiTest.php
@@ -15,22 +15,38 @@ class RegisterApiTest extends TestCase
     // テスト後のデータベースリセット
     use RefreshDatabase;
 
+    private $file;
+    private $datetime_format;
+
+    /**
+     * テスト前処理
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // 画像ファイルの生成
+        $this->file = UploadedFile::fake()->image('test.jpg');
+
+        // 日時フォーマット
+        $this->datetime_format = config('const.DATETIME_FORMAT');
+    }
+
     /**
      * 新しいユーザーを作成して返却する
      * 
      * @test
      * @return void
      */
-    public function createAndReturnNewUser()
+    public function createAndReturnNewUser(): void
     {
-        // 画像ファイルの生成
-        $file = UploadedFile::fake()->image('test.jpg');
-
         // データ
         $data = [
             'name' => 'new user',
             'email' => 'new_user@example.com',
-            'image' => $file,
+            'image' => $this->file,
             'password' => 'test1234',
             'password_confirmation' => 'test1234',
         ];
@@ -42,12 +58,31 @@ class RegisterApiTest extends TestCase
         // 登録したデータ取得
         $user = User::first();
 
+        // 期待値
+        $expected_structure = [
+            'id',
+            'name',
+            'email',
+            'image_path',
+            'updated_at',
+        ];
+        $expected = [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'image_path' => $user->image_path,
+            'updated_at' => $user->updated_at->format($this->datetime_format),
+        ];
+
         // 検証
-        $this->assertEquals($data['name'], $user->name);
-        $response->assertStatus(201)->assertJson(['name' => $user->name]);
-        Storage::disk('public')->assertExists('/user/' . $file->hashName());
+        $response->assertStatus(201)
+            ->assertJsonStructure($expected_structure)
+            ->assertExactJson($expected);
+        $this->assertSame($data['name'], $user->name);
+        $this->assertSame($data['email'], $user->email);
+        Storage::disk('public')->assertExists('/user/' . $this->file->hashName());
 
         // 画像ファイルの削除
-        Storage::disk('public')->delete('/user/' . $file->hashName());
+        Storage::disk('public')->delete('/user/' . $this->file->hashName());
     }
 }
